d201ffb6760696e598c0f13c4c268d8fb7a62154
Ilya <ilya_0836@mail.ru>
2024-02-08 18:53:40 +0300

    v1

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -28,6 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
