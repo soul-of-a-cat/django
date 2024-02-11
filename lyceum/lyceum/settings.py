169fe528678287dccef559ff80852880d9671651
Илья Тимин <timiniljuha@yandex.ru>
2024-02-11 21:34:03 +0000

    Update file settings.py

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -7,7 +7,9 @@ from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 django_stubs_ext.monkeypatch()
+
 load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
