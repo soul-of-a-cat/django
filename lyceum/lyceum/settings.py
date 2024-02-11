933c0949cfb586f5b3f014904d270ff4bf5caeee
Ilya <ilya_0836@mail.ru>
2024-02-11 22:03:37 +0300

    v2

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "False").lower()
+DEBUG = os.environ.get("DJANGO_DEBUG", "False")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
