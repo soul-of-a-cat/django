a802777e756b54bdc6b0679834624fdb5af22bf7
Ilya <ilya_0836@mail.ru>
2024-02-11 23:52:46 +0300

    v2

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,5 +1,6 @@
 from django.test import Client, TestCase
 
+
 # Create your tests here.
 
 
@@ -7,3 +8,8 @@ class StaticUrlTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get("/homepage/")
         self.assertEqual(response.status_code, 200)
+
+    def test_coffee_endpoint(self):
+        response = Client().get("/homepage/coffee/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, "Я чайник")
