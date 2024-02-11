15a4e4bda6a567cd6469e4910c1c9359e11d8cb2
Ilya <ilya_0836@mail.ru>
2024-02-11 21:13:42 +0300

    v2

--- lyceum/manage.py
+++ lyceum/manage.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
-# import sys
-
-import django.core.management.commands.runserver as runserver
+import sys
 
 
 def main():
@@ -17,10 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    cmd = runserver.Command()
-    execute_from_command_line(
-        ["manage.py", "runserver", str(cmd.default_port)]
-    )
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
