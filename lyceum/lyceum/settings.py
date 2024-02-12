7776ff62b391e96c23581cd4fb71eb228d1f6499
Ilya <ilya_0836@mail.ru>
2024-02-12 19:13:01 +0300

    v2

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import dotenv
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(".env")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
