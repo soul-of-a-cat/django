afb414739794260dd8ca8e5b6388faa1a8c53439
Ilya <ilya_0836@mail.ru>
2024-02-14 19:44:10 +0300

    v2

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,14 +1,15 @@
-from rest_framework.test import APITestCase
 from django.test import override_settings
+from rest_framework.test import APITestCase
 
 
 class MiddleWareTests(APITestCase):
-    @override_settings(MIDDLEWARE=("lyceum.middleware.ReverseResponseMiddleware",))
+    @override_settings(
+        MIDDLEWARE=("lyceum.middleware.ReverseResponseMiddleware",)
+    )
     def test_reverse_middleware(self):
         for i in range(10):
             response = self.client.get("/homepage/coffee/")
             if i == 9:
-                # self.assertEqual(response.content.decode("utf-8"), "Я кинйач")
                 self.assertIn("Я кинйач".encode(), response.content)
 
     @override_settings(ALLOW_REVERSE=False)
