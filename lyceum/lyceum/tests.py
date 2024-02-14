7904b7b6b7547e2bec2d0eb6c5c18dcc1072db3d
Ilya <ilya_0836@mail.ru>
2024-02-14 22:14:11 +0300

    v2

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,19 +1,16 @@
-from django.test import override_settings
-from rest_framework.test import APITestCase
+from django.test import Client, override_settings, TestCase
 
 
-class ReverseResponseMiddlewareTests(APITestCase):
+class ReverseResponseMiddlewareTests(TestCase):
     @override_settings(ALLOW_REVERSE=True)
     def test_reverse_russian_words_enabled(self):
-        contents = [
-            self.client.get("/coffee/").content.decode() for _ in range(10)
-        ]
+        client = Client()
+        contents = [client.get("/coffee/").content.decode() for _ in range(10)]
         self.assertIn("Я кинйач", contents)
         self.assertEqual(contents.count("Я кинйач"), 1)
 
     @override_settings(ALLOW_REVERSE=False)
     def test_reverse_russian_words_disabled(self):
-        contents = [
-            self.client.get("/coffee/").content.decode() for _ in range(10)
-        ]
+        client = Client()
+        contents = [client.get("/coffee/").content.decode() for _ in range(10)]
         self.assertNotIn("Я кинйач", contents)
