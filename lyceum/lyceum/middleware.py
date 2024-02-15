09820114e3ba7f64a1e483aefd2cad529d67792e
Илья Тимин <timiniljuha@yandex.ru>
2024-02-15 08:51:42 +0000

    v2

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -9,18 +9,17 @@ class ReverseResponseMiddleware:
     def __init__(self, get_response):
         global count
         self.get_response = get_response
-        self.count = count
 
     def __call__(self, request):
         response = self.get_response(request)
-        if self.count == 9:
+        if count == 9:
             if settings.ALLOW_REVERSE:
                 content = response.content.decode("utf-8")
                 russian_words = re.findall("[а-яА-ЯёЁ]+", content)
                 for word in russian_words:
                     content = content.replace(word, word[::-1])
                 response.content = content.encode("utf-8")
-                self.count = 0
+                count = 0
                 return response
-        self.count += 1
+        count += 1
         return response
