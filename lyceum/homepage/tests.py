612dc2d5049cb3ae07ea7164470eb02d097f7e4b
Ilya <ilya_0836@mail.ru>
2024-02-20 21:29:39 +0300

    v3

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,13 +1,14 @@
-from rest_framework import status
+from http import HTTPStatus
+
 from rest_framework.test import APITestCase
 
 
 class StaticUrlTests(APITestCase):
     def test_homepage_endpoint(self):
         response = self.client.get("/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_coffee_endpoint(self):
         response = self.client.get("/coffee/")
-        self.assertEqual(response.status_code, status.HTTP_418_IM_A_TEAPOT)
+        self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
         self.assertEqual(response.content.decode(), "Я чайник")
