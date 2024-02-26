2fcc26aa128b1cccd95044cc0f162fe3ae3c5d43
Ilya <ilya_0836@mail.ru>
2024-02-26 23:43:25 +0300

    add static files settings

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "lyceum.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,6 +111,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_dev',
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
