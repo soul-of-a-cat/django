67f9fbdc8470ff6268bbe99c0c468b463ed2be9a
Ilya <ilya_0836@mail.ru>
2024-02-28 20:14:22 +0300

    add static settings

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -8,7 +9,7 @@ urlpatterns = [
     path("about/", include("about.urls")),
     path("catalog/", include("catalog.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
