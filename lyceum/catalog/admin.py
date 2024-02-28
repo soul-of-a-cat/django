e1546abdc0bf1e765164a8e53191d0881e9b504a
Ilya <ilya_0836@mail.ru>
2024-02-28 18:09:29 +0300

    add Image field admin

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from sorl.thumbnail.admin import AdminImageMixin
 
 import catalog.models
+
+
+class ItemMainImageInline(AdminImageMixin, admin.TabularInline):
+    model = catalog.models.ItemMainImage
+
+
+class ItemSecondaryImageInline(AdminImageMixin, admin.TabularInline):
+    model = catalog.models.ItemSecondaryImage
 
 
 @admin.register(catalog.models.Item)
@@ -12,6 +21,27 @@ class ItemAdmin(admin.ModelAdmin):
     list_editable = (catalog.models.Item.is_published.field.name,)
     list_display_links = (catalog.models.Item.name.field.name,)
     filter_horizontal = (catalog.models.Item.tags.field.name,)
+
+    inlines = [
+        ItemMainImageInline,
+        ItemSecondaryImageInline,
+    ]
+
+
+@admin.register(catalog.models.ItemMainImage)
+class ItemMainImageAdmin(admin.ModelAdmin):
+    list_display = (
+        catalog.models.ItemMainImage.image_tmb.field_name,
+        catalog.models.ItemMainImage.item.field.name,
+    )
+
+
+@admin.register(catalog.models.ItemSecondaryImage)
+class ItemSecondaryImageAdmin(admin.ModelAdmin):
+    list_display = (
+        catalog.models.ItemSecondaryImage.image_tmb.field_name,
+        catalog.models.ItemSecondaryImage.item.field.name,
+    )
 
 
 @admin.register(catalog.models.Tag)
