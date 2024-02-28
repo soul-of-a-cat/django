972ba396fef80ec66acb8c0b828778081edaf07d
Ilya <ilya_0836@mail.ru>
2024-02-28 18:21:29 +0300

    add connect html and db

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,13 +1,12 @@
-from http import HTTPStatus
-
-from django.http import HttpResponse
-
 from django.shortcuts import render
+
+import catalog.models
 
 
 def item_list(request):
     template = "catalog/item_list.html"
-    context = {}
+    queryset = catalog.models.Item.objects.all()
+    context = {"items": queryset}
     return render(
         request,
         template,
@@ -16,7 +15,27 @@ def item_list(request):
 
 
 def item_detail(request, num):
-    return HttpResponse(
-        "Подробно элемент",
-        status=HTTPStatus.OK,
+    template = "catalog/item.html"
+    item = catalog.models.Item.objects.filter(id=num)
+    if item:
+        category = catalog.models.Category.objects.filter(
+            id=item[0].category_id,
+        )
+        context = {
+            "item_name": item[0].name,
+            "item_text": item[0].text,
+            "category": category.name,
+            "tags": list(item[0].tags.all()),
+        }
+    else:
+        context = {
+            "item_name": "",
+            "item_text": "",
+            "category": "",
+            "tags": "",
+        }
+    return render(
+        request,
+        template,
+        context,
     )
