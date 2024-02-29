3f1002f1261b754720535b2a0cf81388fd083f8e
Ilya <ilya_0836@mail.ru>
2024-02-29 21:57:43 +0300

    add language settings

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -106,13 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = "ru"
+LANGUAGE_CODE = os.getenv("LANGUAGE", default="ru")
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
+
+LOCALE_PATHS = ("locale",)
 
 STATIC_URL = "/static/"
 
