519f8077b86ce377debeca0ecc3c9c2b80f7027d
Ilya <ilya_0836@mail.ru>
2024-03-01 19:10:07 +0300

    rewrite

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -94,178 +94,200 @@ class DBItemTests(TestCase):
 
     @parameterized.parameterized.expand(
         [
-            ("test", "превосходно", True),
-            ("test", "роскошно", True),
-            ("test", "Я превосходно", True),
-            ("test", "превосходно Я", True),
-            ("test", "превосходно роскошно", True),
-            ("test", "роскошно!", True),
-            ("test", "!роскошно", True),
-            ("test", "!роскошно", True),
-            ("test", "роскошно©", True),
-            ("test", "превосходноН", False),
-            ("test", "превНосходно", False),
-            ("test", "Нпревосходно", False),
-            ("test", "Я превосх%одно", False),
-            ("test", "превосходнороскошно", False),
-            ("test" * 38, "превосходно", False),
+            ("test", "превосходно"),
+            ("test", "роскошно"),
+            ("test", "Я превосходно"),
+            ("test", "превосходно Я"),
+            ("test", "превосходно роскошно"),
+            ("test", "роскошно!"),
+            ("test", "!роскошно"),
+            ("test", "!роскошно"),
+            ("test", "роскошно©"),
         ],
     )
-    def test_add_item(self, name, text, is_validate):
+    def test_add_item_no_error(self, name, text):
         item_count = catalog.models.Item.objects.count()
         self.item = catalog.models.Item(
             name=name,
             text=text,
             category=self.category,
         )
-        if not is_validate:
-            with self.assertRaises(ValidationError):
-                self.item.full_clean()
-                self.item.save()
-                self.item.tags.add(self.tag)
-                self.item.full_clean()
-                self.item.save()
-            self.assertEqual(
-                catalog.models.Item.objects.count(),
-                item_count,
-                msg="add no validate item",
-            )
-        else:
+        self.item.full_clean()
+        self.item.save()
+        self.item.tags.add(self.tag)
+        self.item.full_clean()
+        self.item.save()
+        self.assertEqual(
+            catalog.models.Item.objects.count(),
+            item_count + 1,
+            msg="no add validate item",
+        )
+
+    @parameterized.parameterized.expand(
+        [
+            ("test", "превосходноН"),
+            ("test", "превНосходно"),
+            ("test", "Нпревосходно"),
+            ("test", "Я превосх%одно"),
+            ("test", "превосходнороскошно"),
+            ("test" * 38, "превосходно"),
+        ],
+    )
+    def test_add_item_error(self, name, text):
+        item_count = catalog.models.Item.objects.count()
+        self.item = catalog.models.Item(
+            name=name,
+            text=text,
+            category=self.category,
+        )
+        with self.assertRaises(ValidationError):
             self.item.full_clean()
             self.item.save()
             self.item.tags.add(self.tag)
             self.item.full_clean()
             self.item.save()
-            self.assertEqual(
-                catalog.models.Item.objects.count(),
-                item_count + 1,
-                msg="no add validate item",
-            )
+        self.assertEqual(
+            catalog.models.Item.objects.count(),
+            item_count,
+            msg="add no validate item",
+        )
 
 
 class DBTagTests(TestCase):
     @parameterized.parameterized.expand(
         [
-            ("test", "abs", True),
-            ("test", "1abs2", True),
-            ("test", "a12bs", True),
-            ("test", "ABS", True),
-            ("test", "-abs-", True),
-            ("test", "_abs_", True),
-            ("test", "Я", False),
-            ("test", "Яabs", False),
-            ("test", "Я abs", False),
-            ("test", "aЯbs", False),
-            ("test", "absЯ", False),
-            ("test", "abs Я", False),
-            ("test", "*abs*", False),
-            ("test", "a*bs", False),
-            ("test" * 38, "abs", False),
-            ("test", "abs" * 67, False),
+            ("test", "abs"),
+            ("test", "1abs2"),
+            ("test", "a12bs"),
+            ("test", "ABS"),
+            ("test", "-abs-"),
+            ("test", "_abs_"),
         ],
     )
-    def test_add_tag(self, name, slug, is_validate):
+    def test_add_tag_no_error(self, name, slug):
         tag_count = catalog.models.Tag.objects.count()
         self.tag = catalog.models.Tag(
             name=name,
             slug=slug,
         )
-        if not is_validate:
-            with self.assertRaises(ValidationError):
-                self.tag.full_clean()
-                self.tag.save()
-            self.assertEqual(
-                catalog.models.Tag.objects.count(),
-                tag_count,
-                msg="add no validate item",
-            )
-        else:
+        self.tag.full_clean()
+        self.tag.save()
+        self.assertEqual(
+            catalog.models.Tag.objects.count(),
+            tag_count + 1,
+            msg="no add validate item",
+        )
+
+    @parameterized.parameterized.expand(
+        [
+            ("test", "Я"),
+            ("test", "Яabs"),
+            ("test", "Я abs"),
+            ("test", "aЯbs"),
+            ("test", "absЯ"),
+            ("test", "abs Я"),
+            ("test", "*abs*"),
+            ("test", "a*bs"),
+            ("test" * 38, "abs"),
+            ("test", "abs" * 67),
+        ],
+    )
+    def test_add_tag_error(self, name, slug):
+        tag_count = catalog.models.Tag.objects.count()
+        self.tag = catalog.models.Tag(
+            name=name,
+            slug=slug,
+        )
+        with self.assertRaises(ValidationError):
             self.tag.full_clean()
             self.tag.save()
-            self.assertEqual(
-                catalog.models.Tag.objects.count(),
-                tag_count + 1,
-                msg="no add validate item",
-            )
+        self.assertEqual(
+            catalog.models.Tag.objects.count(),
+            tag_count,
+            msg="add no validate item",
+        )
 
 
 class DBCategoryTests(TestCase):
     @parameterized.parameterized.expand(
         [
-            ("test", "abs", 1, True),
-            ("test", "1abs2", 1, True),
-            ("test", "a12bs", 1, True),
-            ("test", "ABS", 1, True),
-            ("test", "-abs-", 1, True),
-            ("test", "_abs_", 1, True),
-            ("test", "_abs_", 32767, True),
-            ("test", "Я", 1, False),
-            ("test", "Яabs", 1, False),
-            ("test", "Я abs", 1, False),
-            ("test", "aЯbs", 1, False),
-            ("test", "absЯ", 1, False),
-            ("test", "abs Я", 1, False),
-            ("test", "*abs*", 1, False),
-            ("test", "a*bs", 1, False),
-            ("test" * 38, "abs", 1, False),
-            ("test", "abs" * 67, 1, False),
-            ("test", "abs", 32768, False),
-            ("test", "abs", 0, False),
-            ("test", "abs", -1, False),
+            ("test", "abs", 1),
+            ("test", "1abs2", 1),
+            ("test", "a12bs", 1),
+            ("test", "ABS", 1),
+            ("test", "-abs-", 1),
+            ("test", "_abs_", 1),
+            ("test", "_abs_", 32767),
         ],
     )
-    def test_add_category(self, name, slug, weight, is_validate):
+    def test_add_category_no_error(self, name, slug, weight):
         category_count = catalog.models.Category.objects.count()
         self.category = catalog.models.Category(
             name=name,
             slug=slug,
             weight=weight,
         )
-        if not is_validate:
-            with self.assertRaises(ValidationError):
-                self.category.full_clean()
-                self.category.save()
-            self.assertEqual(
-                catalog.models.Category.objects.count(),
-                category_count,
-                msg="add no validate item",
-            )
-        else:
+        self.category.full_clean()
+        self.category.save()
+        self.assertEqual(
+            catalog.models.Category.objects.count(),
+            category_count + 1,
+            msg="no add validate item",
+        )
+
+    @parameterized.parameterized.expand(
+        [
+            ("test", "Я", 1),
+            ("test", "Яabs", 1),
+            ("test", "Я abs", 1),
+            ("test", "aЯbs", 1),
+            ("test", "absЯ", 1),
+            ("test", "abs Я", 1),
+            ("test", "*abs*", 1),
+            ("test", "a*bs", 1),
+            ("test" * 38, "abs", 1),
+            ("test", "abs" * 67, 1),
+            ("test", "abs", 32768),
+            ("test", "abs", 0),
+            ("test", "abs", -1),
+        ],
+    )
+    def test_add_category_error(self, name, slug, weight):
+        category_count = catalog.models.Category.objects.count()
+        self.category = catalog.models.Category(
+            name=name,
+            slug=slug,
+            weight=weight,
+        )
+        with self.assertRaises(ValidationError):
             self.category.full_clean()
             self.category.save()
-            self.assertEqual(
-                catalog.models.Category.objects.count(),
-                category_count + 1,
-                msg="no add validate item",
-            )
+        self.assertEqual(
+            catalog.models.Category.objects.count(),
+            category_count,
+            msg="add no validate item",
+        )
 
 
 class DBNormalizeNameTests(TestCase):
     @parameterized.parameterized.expand(
         (
-            (x[0], x[1][:-1], x[1][2])
+            (x[0], x[1])
             for x in itertools.product(
                 [
                     ("test", "1"),
                 ],
                 [
-                    ("test test", "2", True),
-                    ("itfaketest", "2", True),
-                    ("test, test", "2", True),
-                    ("test!test", "2", True),
-                    ("test!", "2", False),
-                    ("!test", "2", False),
-                    ("!test!", "2", False),
-                    (" test ", "2", False),
-                    ("test,", "2", False),
-                    (".test", "2", False),
-                    ("testt", "2", True),
-                    ("te st", "2", False),
+                    ("test test", "2"),
+                    ("itfaketest", "2"),
+                    ("test, test", "2"),
+                    ("test!test", "2"),
+                    ("testt", "2"),
                 ],
             )
         ),
     )
-    def test_add_item(self, data1, data2, is_validate):
+    def test_add_item_normalize_name_no_error(self, data1, data2):
         self.tag = catalog.models.Tag(
             name=data1[0],
             slug=data1[1],
@@ -277,27 +299,50 @@ class DBNormalizeNameTests(TestCase):
             name=data2[0],
             slug=data2[1],
         )
-        with self.subTest(
-            name1=data1[0],
-            slug1=data1[1],
-            name2=data2[0],
-            slug2=data2[1],
-            is_validate=is_validate,
-        ):
-            if not is_validate:
-                with self.assertRaises(ValidationError):
-                    self.tag.full_clean()
-                    self.tag.save()
-                self.assertEqual(
-                    catalog.models.Tag.objects.count(),
-                    tag_count,
-                    msg="add no validate item",
-                )
-            else:
-                self.tag.full_clean()
-                self.tag.save()
-                self.assertEqual(
-                    catalog.models.Tag.objects.count(),
-                    tag_count + 1,
-                    msg="no add validate item",
-                )
+        self.tag.full_clean()
+        self.tag.save()
+        self.assertEqual(
+            catalog.models.Tag.objects.count(),
+            tag_count + 1,
+            msg="no add validate item",
+        )
+
+    @parameterized.parameterized.expand(
+        (
+            (x[0], x[1])
+            for x in itertools.product(
+                [
+                    ("test", "1"),
+                ],
+                [
+                    ("test!", "2"),
+                    ("!test", "2"),
+                    ("!test!", "2"),
+                    (" test ", "2"),
+                    ("test,", "2"),
+                    (".test", "2"),
+                    ("te st", "2"),
+                ],
+            )
+        ),
+    )
+    def test_add_item_normalize_name_error(self, data1, data2):
+        self.tag = catalog.models.Tag(
+            name=data1[0],
+            slug=data1[1],
+        )
+        self.tag.full_clean()
+        self.tag.save()
+        tag_count = catalog.models.Tag.objects.count()
+        self.tag = catalog.models.Tag(
+            name=data2[0],
+            slug=data2[1],
+        )
+        with self.assertRaises(ValidationError):
+            self.tag.full_clean()
+            self.tag.save()
+        self.assertEqual(
+            catalog.models.Tag.objects.count(),
+            tag_count,
+            msg="add no validate item",
+        )
