a84d90c978424db2e737fa1256cad4b681c36bc5
Ilya <ilya_0836@mail.ru>
2024-03-06 15:28:27 +0300

    update add img

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -35,7 +35,9 @@ def item_detail(request, num):
         .prefetch_related(
             django.db.models.Prefetch(
                 "images",
-                queryset=catalog.models.ItemSecondaryImage.objects.only("image"),
+                queryset=catalog.models.ItemSecondaryImage.objects.only(
+                    "image",
+                ),
             ),
         )
         .only("name", "category__name", "text", "main_image__image"),
