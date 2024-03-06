c74d9695735dd556f8fe14353aee0979afc930a2
Ilya <ilya_0836@mail.ru>
2024-03-06 21:25:59 +0300

    rewrite

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -9,6 +9,7 @@ __all__ = ["item_list", "item_detail"]
 def item_list(request):
     template = "catalog/item_list.html"
     items = catalog.models.Item.objects.published().order_by("category__name")
+
     context = {"items": items}
     return render(
         request,
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -13,11 +13,7 @@ __all__ = [
 
 def home(request):
     template = "homepage/main.html"
-    items = (
-        catalog.models.Item.objects.published()
-        .filter(is_on_main=True)
-        .order_by("name")
-    )
+    items = catalog.models.Item.objects.on_main().order_by("name")
 
     context = {"items": items}
     return render(
