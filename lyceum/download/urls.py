d19c2ae2bbd53373ee9467d52019c951c4071f5a
Ilya <ilya_0836@mail.ru>
2024-03-07 23:45:15 +0300

    rewrite

--- lyceum/download/urls.py
+++ lyceum/download/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-import Download.views
+import download.views
 
 app_name = "download"
 urlpatterns = [
     path(
         "<int:main_image_id>/",
-        Download.views.download_main_image,
+        download.views.download_main_image,
         name="download_main_image",
     ),
 ]
