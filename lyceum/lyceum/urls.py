0adef9338bdc62efb116920e03cace619a6ea812
Ilya <ilya_0836@mail.ru>
2024-03-07 21:55:19 +0300

    add Download

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("catalog/", include("catalog.urls")),
     path("admin/", admin.site.urls),
     path("mdeditor/", include("mdeditor.urls")),
+    path("download/", include("Download.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
