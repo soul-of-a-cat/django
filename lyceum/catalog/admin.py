36c7f515a00941b4709f324fd6b7d78f155ac97e
Ilya <ilya_0836@mail.ru>
2024-03-07 23:31:41 +0300

    add readonly fields

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -36,6 +36,10 @@ class ItemAdmin(admin.ModelAdmin):
     list_editable = (catalog.models.Item.is_published.field.name,)
     list_display_links = (catalog.models.Item.name.field.name,)
     filter_horizontal = (catalog.models.Item.tags.field.name,)
+    readonly_fields = (
+        catalog.models.Item.created.field.name,
+        catalog.models.Item.updated.field.name,
+    )
 
     inlines = [
         ItemMainImageInline,
