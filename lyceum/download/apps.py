3b5815f9c5201e9ba420220d4341f300ffedcfa4
Ilya <ilya_0836@mail.ru>
2024-03-07 23:32:07 +0300

    rewrite

--- lyceum/download/apps.py
+++ lyceum/download/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
-__all__ = ["DownloadConfig"]
+__all__ = [
+    "DownloadConfig",
+]
 
 
 class DownloadConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "Download"
+    name = "download"
     verbose_name = "Скачать"
