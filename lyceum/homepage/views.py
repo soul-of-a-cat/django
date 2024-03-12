9dd79d5d8bf1e1aa561cae1636b5656071657cde
Ilya <ilya_0836@mail.ru>
2024-03-12 21:31:21 +0300

    add echo views

--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 import catalog.models
+from homepage.forms import HomepageForm
 
 __all__ = [
     "home",
@@ -29,4 +30,37 @@ def coffee(request):
     return HttpResponse(
         "Я чайник",
         status=HTTPStatus.IM_A_TEAPOT,
+    )
+
+
+def echo(request):
+    template = "homepage/echo.html"
+    form = HomepageForm(request.POST or None)
+
+    if form.is_valid():
+        text = form.cleaned_data.get("text")
+
+        return HttpResponse(
+            text,
+            content_type="text/plain",
+        )
+
+    context = {
+        "form": form,
+    }
+
+    return render(
+        request,
+        template,
+        context,
+    )
+
+
+def echo_submit(request):
+
+    text = request.POST.get("text")
+
+    return HttpResponse(
+        text,
+        content_type="text/plain",
     )
