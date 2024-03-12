868a1afd8d85d79a9673a1d8b4443ee26568ff09
Ilya <ilya_0836@mail.ru>
2024-03-12 21:29:56 +0300

    add feedback test

---
+++ lyceum/feedback/tests.py
@@ -0,0 +1,37 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+import feedback.forms
+
+__all__ = [
+    "FormTests",
+]
+
+
+class FormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.form = feedback.forms.FeedbackForm()
+
+    def test_name_label(self):
+        name_label = self.form.fields["text"].label
+        self.assertEqual(name_label, "Текст")
+
+    def test_name_help_text(self):
+        name_help_text = self.form.fields["text"].help_text
+        self.assertEqual(name_help_text, "Напишите текст сообщения")
+
+    def test_create_task(self):
+        form_data = {
+            "text": "test",
+            "mail": "sgadfh@mail.ru",
+        }
+
+        response = Client().post(
+            reverse("feedback:feedback"),
+            form_data,
+            follow=True,
+        )
+
+        self.assertRedirects(response, reverse("feedback:feedback"))
