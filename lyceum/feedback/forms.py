b9cb33b4b56d83ddd3417b5e7c3e7de66bcf8d46
Ilya <ilya_0836@mail.ru>
2024-03-13 00:06:54 +0300

    add error messages

--- lyceum/feedback/forms.py
+++ lyceum/feedback/forms.py
@@ -9,14 +9,23 @@ class FeedbackForm(forms.Form):
     name = forms.CharField(
         label="Имя пользователя",
         help_text="Напишите своё имя",
+        error_messages={
+            'required': 'Please enter your name'
+        },
     )
     text = forms.CharField(
         label="Текст",
         help_text="Напишите текст сообщения",
-        widget=forms.Textarea,
+        widget=forms.Textarea(attrs={"class": "my-field"}),
+        error_messages={
+            'required': 'Please enter text'
+        },
     )
     mail = forms.EmailField(
         label="Почта",
         max_length=100,
         help_text="Почтовый адрес",
+        error_messages={
+            'required': 'Please enter your email'
+        },
     )
