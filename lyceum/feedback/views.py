328a4d583e33ec9f5aa30da2e1b1fc00ef61115b
Ilya <ilya_0836@mail.ru>
2024-03-12 21:31:15 +0300

    add feedback views

---
+++ lyceum/feedback/views.py
@@ -0,0 +1,41 @@
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import redirect, render
+
+from feedback.forms import FeedbackForm
+
+
+__all__ = [
+    "feedback",
+]
+
+
+def feedback(request):
+    template = "feedback/feedback.html"
+    form = FeedbackForm(request.POST or None)
+
+    if form.is_valid():
+        text = form.cleaned_data.get("text")
+        user_mail = form.cleaned_data.get("mail")
+        django_mail = settings.EMAIL_HOST_USER
+        send_mail(
+            "Subject here",
+            text,
+            django_mail,
+            [user_mail],
+        )
+
+        messages.success(request, "Форма успешно отправлена!")
+
+        return redirect("feedback:feedback")
+
+    context = {
+        "form": form,
+    }
+
+    return render(
+        request,
+        template,
+        context,
+    )
