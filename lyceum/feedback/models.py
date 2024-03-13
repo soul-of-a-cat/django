f78d50da8d1e1beb023a34b96220ac3c723737eb
Ilya <ilya_0836@mail.ru>
2024-03-13 23:06:12 +0300

    delete FeedbackForm

--- lyceum/feedback/models.py
+++ lyceum/feedback/models.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.db import models
 
 __all__ = [
@@ -46,49 +45,3 @@ class Feedback(models.Model):
 
     def __str__(self):
         return self.text[:10]
-
-
-class FeedbackForm(forms.ModelForm):
-    class Meta:
-        model = Feedback
-
-        exclude = [
-            Feedback.created_on.field.name,
-            Feedback.status.field.name,
-        ]
-
-        fields = (
-            Feedback.name.field.name,
-            Feedback.text.field.name,
-            Feedback.mail.field.name,
-        )
-
-        labels = {
-            Feedback.name.field.name: "Имя",
-            Feedback.text.field.name: "Текст",
-            Feedback.mail.field.name: "Почта",
-        }
-
-        help_texts = {
-            Feedback.name.field.name: "Напишите своё имя",
-            Feedback.text.field.name: "Напишите текст сообщения",
-            Feedback.mail.field.name: "Почтовый адрес",
-        }
-
-        widgets = {
-            Feedback.text.field.name: forms.TextInput(
-                attrs={"class": "my-field"},
-            ),
-        }
-
-        error_messages = {
-            Feedback.name.field.name: {
-                "required": "Please enter your name",
-            },
-            Feedback.text.field.name: {
-                "required": "Please enter text",
-            },
-            Feedback.mail.field.name: {
-                "required": "Please enter your email",
-            },
-        }
