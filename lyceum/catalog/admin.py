3215a31b2ea0f0f466274de5359fb02c17f49afd
Ilya <ilya_0836@mail.ru>
2024-03-14 19:27:06 +0300

    add FeedbackAdmin

--- lyceum/catalog/admin.py
+++ lyceum/catalog/admin.py
@@ -4,6 +4,7 @@ from mdeditor.widgets import MDEditorWidget
 from sorl.thumbnail.admin import AdminImageMixin
 
 import catalog.models
+import feedback.models
 
 __all__ = [
     "CategoryAdmin",
@@ -90,3 +91,41 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         catalog.models.Category.slug.field.name: (category_name,),
     }
+
+
+@admin.register(feedback.models.Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = (
+        feedback.models.Feedback.text.field.name,
+        feedback.models.Feedback.mail.field.name,
+        feedback.models.Feedback.status.field.name,
+    )
+    list_display_links = (feedback.models.Feedback.text.field.name,)
+    readonly_fields = (
+        feedback.models.Feedback.name.field.name,
+        feedback.models.Feedback.text.field.name,
+        feedback.models.Feedback.created_on.field.name,
+        feedback.models.Feedback.mail.field.name,
+    )
+    fieldsets = (
+        (None,
+         {
+             'fields': (
+                 feedback.models.Feedback.name.field.name,
+                 feedback.models.Feedback.text.field.name,
+                 feedback.models.Feedback.mail.field.name,
+                 feedback.models.Feedback.status.field.name,
+                 feedback.models.Feedback.created_on.field.name,
+             )
+         }
+         ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        feedback.models.StatusLog.objects.create(
+            user=request.user,
+            from_status=feedback.models.Feedback.objects.get(id=obj.id).status,
+            to_status=obj.status,
+        )
+        super().save_model(request, obj, form, change)
