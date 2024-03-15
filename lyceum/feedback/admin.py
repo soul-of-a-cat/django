6fe2d3930223bd18a2f8b2027429db3496d0cd45
Ilya <ilya_0836@mail.ru>
2024-03-15 20:23:39 +0300

    add StatusLogAdmin

--- lyceum/feedback/admin.py
+++ lyceum/feedback/admin.py
@@ -4,36 +4,63 @@ import feedback.models
 
 __all__ = [
     "FeedbackAdmin",
+    "StatusLogAdmin",
 ]
+
+
+class FeedbackAuthorInline(admin.TabularInline):
+    model = feedback.models.FeedbackAuthor
+    can_delete = False
+
+
+class FeedbackFileInline(admin.TabularInline):
+    model = feedback.models.FeedbackFile
 
 
 @admin.register(feedback.models.Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = (
         feedback.models.Feedback.text.field.name,
-        feedback.models.Feedback.mail.field.name,
-        feedback.models.Feedback.status.field.name,
-    )
-    list_display_links = (feedback.models.Feedback.text.field.name,)
-    readonly_fields = (
-        feedback.models.Feedback.name.field.name,
-        feedback.models.Feedback.text.field.name,
         feedback.models.Feedback.created_on.field.name,
-        feedback.models.Feedback.mail.field.name,
-    )
-    fields = (
-        feedback.models.Feedback.name.field.name,
-        feedback.models.Feedback.text.field.name,
-        feedback.models.Feedback.mail.field.name,
         feedback.models.Feedback.status.field.name,
-        feedback.models.Feedback.created_on.field.name,
     )
 
+    list_display_links = (feedback.models.Feedback.text.field.name,)
+
+    fields = (
+        feedback.models.Feedback.created_on.field.name,
+        feedback.models.Feedback.text.field.name,
+        feedback.models.Feedback.status.field.name,
+    )
+
+    readonly_fields = (feedback.models.Feedback.created_on.field.name,)
+
+    inlines = [FeedbackAuthorInline, FeedbackFileInline]
+
     def save_model(self, request, obj, form, change):
-        obj.user = request.user
-        feedback.models.StatusLog.objects.create(
-            user=request.user,
-            from_status=feedback.models.Feedback.objects.get(id=obj.id).status,
-            to_status=obj.status,
-        )
+        if "status" in form.changed_data:
+            feedback.models.StatusLog(
+                user=request.user,
+                feedback=obj,
+                _from=form.initial["status"],
+                to=form.cleaned_data["status"],
+            ).save()
+
         super().save_model(request, obj, form, change)
+
+
+@admin.register(feedback.models.StatusLog)
+class StatusLogAdmin(admin.ModelAdmin):
+    list_display = (
+        feedback.models.StatusLog.user.field.name,
+        feedback.models.StatusLog.timestamp.field.name,
+        feedback.models.StatusLog.from_status.field.name,
+        feedback.models.StatusLog.to.field.name,
+    )
+    readonly_fields = (
+        feedback.models.StatusLog.user.field.name,
+        feedback.models.StatusLog.timestamp.field.name,
+        feedback.models.StatusLog.from_status.field.name,
+        feedback.models.StatusLog.to.field.name,
+        feedback.models.StatusLog.feedback.field.name,
+    )
