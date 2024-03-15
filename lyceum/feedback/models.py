ed874441e6d5c43fd8d2af24d0f6816f54967da2
Ilya <ilya_0836@mail.ru>
2024-03-15 20:27:37 +0300

    add model FeedbackFile

--- lyceum/feedback/models.py
+++ lyceum/feedback/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 __all__ = [
     "Feedback",
+    "StatusLog",
+    "FeedbackAuthor",
+    "FeedbackFile",
+    "Status",
 ]
 
 
@@ -13,43 +17,29 @@ class Status(models.TextChoices):
 
 
 class Feedback(models.Model):
-    name = models.CharField(
-        "имя",
-        help_text="Напишите своё имя",
-        max_length=100,
-        null=True,
-        blank=True,
-    )
-    text = models.CharField(
-        "текст",
-        help_text="Напишите текст сообщения",
-        max_length=1000,
+    text = models.TextField(
+        verbose_name="текст",
+        help_text="напишите текст сообщения",
     )
     created_on = models.DateTimeField(
         verbose_name="создано",
-        help_text="Дата и время создания",
+        help_text="дата и время создания",
         auto_now_add=True,
         null=True,
     )
-    mail = models.EmailField(
-        verbose_name="e-mail",
-        help_text="Почтовый адрес",
-    )
     status = models.CharField(
-        verbose_name="status",
-        help_text="Статус",
-        max_length=100,
         choices=Status.choices,
         default=Status.RECEIVED,
+        max_length=11,
+        verbose_name="статус",
     )
 
     class Meta:
-        app_label = "feedback"
         verbose_name = "обратная связь"
         verbose_name_plural = "обратные связи"
 
-    def __str__(self):
-        return self.text[:10]
+    def __str__(self) -> str:
+        return f"обратная связь ({self.id})"
 
 
 class StatusLog(models.Model):
@@ -57,27 +47,78 @@ class StatusLog(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True,
-        verbose_name="пользователь",
+    )
+    feedback = models.ForeignKey(
+        Feedback,
+        on_delete=models.SET_NULL,
+        null=True,
     )
     timestamp = models.DateTimeField(
         verbose_name="создано",
-        help_text="Дата и время создания",
+        help_text="дата и время создания",
         auto_now_add=True,
         null=True,
     )
     from_status = models.CharField(
-        verbose_name="status",
-        help_text="Статус",
-        max_length=100,
         choices=Status.choices,
-        null=True,
         db_column="from",
+        max_length=11,
+        verbose_name="с",
     )
     to = models.CharField(
-        verbose_name="status",
-        help_text="Статус",
-        max_length=100,
         choices=Status.choices,
-        null=True,
-        db_column="to",
+        max_length=11,
+        verbose_name="на",
     )
+
+    class Meta:
+        verbose_name = "журнал состояния"
+        verbose_name_plural = "журнал состояний"
+
+    def __str__(self) -> str:
+        return f"состояние ({self.id})"
+
+
+class FeedbackFile(models.Model):
+    def upload_to(self, filename):
+        return f"uploads/{self.feedback_id}/{filename}"
+
+    feedback = models.ForeignKey(
+        Feedback,
+        on_delete=models.CASCADE,
+        verbose_name="обратная связь",
+        related_name="files",
+        related_query_name="files",
+        help_text="файлы",
+    )
+    file = models.FileField(
+        upload_to=upload_to,
+    )
+
+    class Meta:
+        verbose_name = "файлы обратной связи"
+        verbose_name_plural = "файлы обратных связей"
+
+
+class FeedbackAuthor(models.Model):
+    name = models.CharField(
+        verbose_name="имя",
+        max_length=150,
+        help_text="напишите имя",
+        null=True,
+        blank=True,
+    )
+    mail = models.EmailField(
+        verbose_name="почта",
+        help_text="почтовый адрес",
+    )
+    feedback = models.OneToOneField(
+        Feedback,
+        on_delete=models.CASCADE,
+        related_name="author",
+        related_query_name="author",
+    )
+
+    class Meta:
+        verbose_name = "данные автора"
+        verbose_name_plural = "данные авторов"
