4400b75131001fdea3d0429da9f1858757e85fcb
Ilya <ilya_0836@mail.ru>
2024-03-21 21:57:41 +0300

    add user backend

---
+++ lyceum/users/backends.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.backends import get_user_model, ModelBackend
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import Q
+
+__all__ = [
+    "UserModelBackend",
+]
+
+UserModel = get_user_model()
+
+
+class UserModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserModel.objects.get(
+                Q(username=username) | Q(email__iexact=username)
+            )
+        except UserModel.DoesNotExist:
+            return None
+
+        except MultipleObjectsReturned:
+            return (
+                UserModel.objects.filter(email=username).order_by("id").first()
+            )
+        else:
+            if user.check_password(password) and self.user_can_authenticate(
+                user
+            ):
+                return user
+
+    def get_user(self, user_id):
+        try:
+            user = UserModel.objects.get(pk=user_id)
+        except UserModel.DoesNotExist:
+            return None
+
+        return user if self.user_can_authenticate(user) else None
