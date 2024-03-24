55bcf214d446bf96903df3b22f13fd95bbb8c204
Ilya <ilya_0836@mail.ru>
2024-03-24 16:29:59 +0300

    rewrite with black

--- lyceum/users/backends.py
+++ lyceum/users/backends.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.backends import ModelBackend, get_user_model
+from django.contrib.auth.backends import get_user_model, ModelBackend
 from django.core.exceptions import MultipleObjectsReturned
 from django.db.models import Q
+
+__all__ = [
+    "UserModelBackend",
+]
 
 UserModel = get_user_model()
 
@@ -8,13 +12,19 @@ UserModel = get_user_model()
 class UserModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user = UserModel.objects.get(Q(username=username) | Q(email__iexact=username))
+            user = UserModel.objects.get(
+                Q(username=username) | Q(email__iexact=username)
+            )
         except UserModel.DoesNotExist:
             return None
         except MultipleObjectsReturned:
-            return UserModel.objects.filter(email=username).order_by('id').first()
+            return (
+                UserModel.objects.filter(email=username).order_by("id").first()
+            )
         else:
-            if user.check_password(password) and self.user_can_authenticate(user):
+            if user.check_password(password) and self.user_can_authenticate(
+                user
+            ):
                 return user
 
     def get_user(self, user_id):
