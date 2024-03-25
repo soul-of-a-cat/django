ae7cfe0ed2195941df417029b5a017a80358c4f2
Ilya <ilya_0836@mail.ru>
2024-03-25 17:30:28 +0300

    add attempts_count in model

--- lyceum/users/models.py
+++ lyceum/users/models.py
@@ -1,12 +1,7 @@
-from pathlib import Path
-import sys
-from typing import cast, Optional
-import uuid
-
-from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
+import django.contrib.auth.models
+from django.contrib.auth.models import User as AuthUser
 from django.db import models
+import sorl.thumbnail
 
 
 __all__ = [
@@ -15,17 +10,14 @@ __all__ = [
     "UserManager",
 ]
 
-if "makemigrations" not in sys.argv and "migrate" not in sys.argv:
-    User._meta.get_field("email")._unique = True
-
-
-def get_path_image(instance, filename):
-    ext = Path(filename).suffix
-    return f"users/{uuid.uuid4()}{ext}"
+AuthUser._meta.get_field("email")._unique = True
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def image_path(self, filename):
+        return f"users/{self.user.id}/{filename}"
+
+    user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
     birthday = models.DateField(
         "Дата рождения",
         blank=True,
@@ -35,25 +27,47 @@ class Profile(models.Model):
     coffee_count = models.PositiveIntegerField(
         "Сколько раз пользователь пил кофе",
         default=0,
-        validators=[MinValueValidator(0)],
         help_text="Количество переходов по /coffee/",
-        null=True,
+    )
+    attempts_count = models.PositiveIntegerField(
+        "Попытки входа",
+        default=0,
+    )
+    block_date = models.DateTimeField(
+        "Дата блокировки",
         blank=True,
+        null=True,
     )
     image = models.ImageField(
         "аватарка",
         help_text="Загрузите автарку",
-        upload_to=get_path_image,
+        upload_to=image_path,
         null=True,
         blank=True,
     )
+
+    def get_image_300x300(self):
+        return sorl.thumbnail.get_thumbnail(
+            self.image,
+            "300x300",
+            crop="center",
+            quality=51,
+        )
 
     class Meta:
         verbose_name = "Дополнительное поле"
         verbose_name_plural = "Дополнительные поля"
 
 
-class UserManager(models.Manager):
+class UserManager(django.contrib.auth.models.UserManager):
+    CANONICAL_DOMAINS = {
+        "ya.ru": "yandex.ru",
+    }
+    DOTS = {
+        "yandex.ru": "-",
+        "gmail.com": "",
+    }
+
     def get_queryset(self) -> models.query.QuerySet:
         return (
             super()
@@ -63,17 +77,34 @@ class UserManager(models.Manager):
             )
         )
 
-    def active(self) -> models.query.QuerySet:
+    def active(self):
         return self.get_queryset().filter(is_active=True)
 
-    def by_mail(self, mail: str) -> "User | None":
-        return cast(Optional[User], self.get_queryset().get(email=mail))
+    def by_mail(self, mail):
+        normalize_email = self.normalize_email(mail)
+        return self.active().get(email=normalize_email)
 
-    def normalize_email(self, mail):
-        return mail
+    @classmethod
+    def normalize_email(cls, email):
+        email = super().normalize_email(email).lower()
+        try:
+            email_name, domain_part = email.strip().rsplit("@", 1)
+            email_name, _ = email_name.split("+", 1)
+
+            domain_part = cls.CANONICAL_DOMAINS.get(domain_part, domain_part)
+
+            email_name = email_name.replace(
+                ".", cls.DOTS.get(domain_part, ".")
+            )
+        except ValueError:
+            pass
+        else:
+            email = "@".join([email_name, domain_part.lower()])
+
+        return email
 
 
-class User(get_user_model()):
+class User(AuthUser):
     objects = UserManager()
 
     class Meta:
