6f28089e5e46b565e9dde19365d4b52b4a5f15fe
Smer4k <151877040+Smer4k@users.noreply.github.com>
2024-03-28 00:11:34 +0300

    feat: js filter by local time

--- lyceum/lyceum/context_processors.py
+++ lyceum/lyceum/context_processors.py
@@ -2,11 +2,13 @@ from datetime import datetime
 
 from users.models import User
 
+__all__ = []
+
 
 def birthday_users(request):
     today = datetime.now().date()
     birthday_users = User.objects.filter(
         profile__birthday__day=today.day, profile__birthday__month=today.month
-    )
+    ).only("profile__birthday", "username")
 
     return {"birthday_users": birthday_users}
