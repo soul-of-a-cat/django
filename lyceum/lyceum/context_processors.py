14ff35c3cd3dcc6d9ab0040c0d02e6ab63787fec
Smer4k <151877040+Smer4k@users.noreply.github.com>
2024-03-28 01:03:01 +0300

    update context_processors.py, header.html

--- lyceum/lyceum/context_processors.py
+++ lyceum/lyceum/context_processors.py
@@ -8,7 +8,9 @@ __all__ = []
 def birthday_users(request):
     today = datetime.now().date()
     birthday_users = User.objects.filter(
-        profile__birthday__day=today.day, profile__birthday__month=today.month
-    ).only("profile__birthday", "username")
+        profile__birthday__day=today.day,
+        profile__birthday__month=today.month,
+        is_active=True,
+    ).only("profile__birthday", "username", "email")
 
     return {"birthday_users": birthday_users}
